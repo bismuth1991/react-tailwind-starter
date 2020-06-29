826123f2b2188c637d2c6bf5f1d82141713b4393
bismuth1991 <pbao.tran.unsw@gmail.com>
2020-06-29 16:32:53 -0400

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <header className="font-proxima text-4xl text-primary font-bold">
+      Hello from Tailwindcss
+    </header>
+  );
+};
+
+export default App;
